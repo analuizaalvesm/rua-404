14d05186c3d3bed27fb9c0e6b233c40a85b52c31
Lucas xae <lucas.hcb0405@gmail.com>
2024-09-10 18:48:48 -0300

    create Rua404 back end project and connect with Database

---
+++ Codigo/Rua404/Rua404.Domain/Entities/Teste.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Rua404.Domain.Entities
+{
+    //[Table("teste")]
+    public class Teste
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Codigo/Rua404/Rua404.Infraestrutura/Rua404DbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Rua404.Domain.Entities;
+
+namespace Rua404.Infraestrutura
+{
+    public class Rua404DbContext : DbContext
+    {
+        private IConfiguration _configuration;
+
+        public DbSet<Teste> Teste { get; set; }
+        public Rua404DbContext(IConfiguration configuration, DbContextOptions options) : base(options)
+        { 
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var typeDatabase = _configuration["TypeDatabase"];
+            var connectionString = _configuration.GetConnectionString(typeDatabase);
+
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+}
