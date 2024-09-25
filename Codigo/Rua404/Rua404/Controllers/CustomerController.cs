7b57a138385bef525ea2879d03ced4ceab0869b4
Matheus Porto <matheus5299porto@hotmail.com>
2024-09-25 13:24:03 -0300

    Adicionandoc CRUD de usuarios

---
+++ Codigo/Rua404/Rua404/Controllers/CustomerController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rua404.Domain.Entities;
+using Rua404.Infraestrutura;
+
+namespace Rua404.Controllers
+{
+    [ApiController]
+    [Route("api/controller")]
+    public class CustomerController : Controller
+    {
+        private readonly Rua404DbContext _context;
+
+        public CustomerController(Rua404DbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(short id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCustomer), new { id = customer.customer_id }, customer);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomer(short id, Customer customer)
+        {
+            if (id != customer.customer_id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(short id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CustomerExists(short id)
+        {
+            return _context.Customers.Any(e => e.customer_id== id);
+        }
+    }
+}
