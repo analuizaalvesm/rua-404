1234f5da038d326fd97a5c16cbbc44f0de22a9f2
Lucas xae <lucas.hcb0405@gmail.com>
2024-10-07 21:26:43 -0300

    new hc route

--- Codigo/Rua-404-Teste/Rua404/src/main/java/org/example/Controller/HealthCheckController.java
+++ Codigo/Rua-404-Teste/Rua404/src/main/java/org/example/Controller/HealthCheckController.java
@@ -1,18 +1,15 @@
 package org.example.Controller;
 
-import org.apache.catalina.connector.Response;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("auth")
-public class AuthController {
+@RequestMapping("hc")
+public class HealthCheckController {
 
-
-    @GetMapping("/login")
+    @GetMapping
     public String login() {
-        return "login";
+        return "It's working!";
     }
 }
