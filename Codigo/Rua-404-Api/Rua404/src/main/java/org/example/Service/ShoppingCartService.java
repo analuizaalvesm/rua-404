5ebe444ca67246e4a82742f03e79f7c30f80878e
Lucas xae <lucas.hcb0405@gmail.com>
2024-11-01 20:47:37 -0300

    Finalizando rotas de carrinho - CRUD completo

--- Codigo/Rua-404-Api/Rua404/src/main/java/org/example/Service/ShoppingCartService.java
+++ Codigo/Rua-404-Api/Rua404/src/main/java/org/example/Service/ShoppingCartService.java
@@ -1,7 +1,6 @@
 package org.example.Service;
 
-import org.example.Model.Customer;
-import org.example.Model.Pedido;
+import org.example.Model.ShoppingCart;
 import org.example.Repositories.ShoppingCartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,31 +14,28 @@ public class ShoppingCartService {
     @Autowired
     private ShoppingCartRepository shoppingCartRepository;
 
-    public List<Pedido> get(){
+    public List<ShoppingCart> get(){
         return shoppingCartRepository.findAll();
     }
 
-    public List<Pedido> getByEmail(String email){
+    public List<ShoppingCart> getByUserId(String userId){
         try{
-            return shoppingCartRepository.findAllByEmail(email);
+            return shoppingCartRepository.findAllByUserID(userId);
         }catch(Exception e){
             return null;
-                    //List<HttpStatus.BAD_REQUEST.toString()>; //retorna um erro 400
         }
     }
 
-//    public String post(Pedido pedido){
-//       try{
-//           if(pedido.getEmail() == null){
-//               return HttpStatus.BAD_REQUEST.toString();
-//           }
-//           shoppingCartRepository.save(pedido);
-//           return HttpStatus.OK + pedido.toString();
-//
-//       }catch (Exception e){
-//           return HttpStatus.BAD_REQUEST.toString();
-//       }
-//    }
+    public String post(ShoppingCart pedido){
+       try{
+
+           shoppingCartRepository.save(pedido);
+           return HttpStatus.OK + pedido.toString();
+
+       }catch (Exception e){
+           return HttpStatus.BAD_REQUEST.toString();
+       }
+    }
 
     public String delete(Long id){
         try{
@@ -50,7 +46,7 @@ public class ShoppingCartService {
         }
     }
 
-    public String put(Pedido pedido){
+    public String put(ShoppingCart pedido){
         try {
             shoppingCartRepository.save(pedido);
             return HttpStatus.OK.toString();
