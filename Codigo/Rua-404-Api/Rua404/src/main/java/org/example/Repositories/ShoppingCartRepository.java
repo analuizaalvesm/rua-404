d5749b6a02977cb0f265d7a7c4ffa7fd30470d25
analuizaalvesm <analuizaalvesm@gmail.com>
2024-11-02 23:08:51 -0300

    Revert "Merge branch 'master' of https://github.com/ICEI-PUC-Minas-PPLES-TI/plf-es-2024-2-ti4-1254100-rua-4-0-4 into feature/sprint-4"
    This reverts commit a81ac99b292f9a967b75a40f573137337a206413, reversing
    changes made to 0ad1e7f9017125343e1d645b708b3802046a1b69.

--- Codigo/Rua-404-Api/Rua404/src/main/java/org/example/Repositories/ShoppingCartRepository.java
+++ Codigo/Rua-404-Api/Rua404/src/main/java/org/example/Repositories/ShoppingCartRepository.java
@@ -1,6 +1,7 @@
 package org.example.Repositories;
 
-import org.example.Model.ShoppingCart;
+import org.example.Model.Order;
+import org.example.Model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,8 +9,8 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+public interface ShoppingCartRepository extends JpaRepository<Pedido, Long> {
 
-    @Query("SELECT u FROM ShoppingCart u WHERE u.user.customer_id = :id")
-    List<ShoppingCart> findAllByUserID(String id);
+    @Query("SELECT u FROM Pedido u WHERE u.email = :email")
+    public List<Pedido> findAllByEmail(String email);
 }
--- Codigo/Rua-404-Api/Rua404/src/main/java/org/example/Service/ShoppingCartService.java
+++ Codigo/Rua-404-Api/Rua404/src/main/java/org/example/Service/ShoppingCartService.java
@@ -1,6 +1,7 @@
 package org.example.Service;
 
-import org.example.Model.ShoppingCart;
+import org.example.Model.Customer;
+import org.example.Model.Pedido;
 import org.example.Repositories.ShoppingCartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,28 +15,31 @@ public class ShoppingCartService {
     @Autowired
     private ShoppingCartRepository shoppingCartRepository;
 
-    public List<ShoppingCart> get(){
+    public List<Pedido> get(){
         return shoppingCartRepository.findAll();
     }
 
-    public List<ShoppingCart> getByUserId(String userId){
+    public List<Pedido> getByEmail(String email){
         try{
-            return shoppingCartRepository.findAllByUserID(userId);
+            return shoppingCartRepository.findAllByEmail(email);
         }catch(Exception e){
             return null;
+                    //List<HttpStatus.BAD_REQUEST.toString()>; //retorna um erro 400
         }
     }
 
-    public String post(ShoppingCart pedido){
-       try{
-
-           shoppingCartRepository.save(pedido);
-           return HttpStatus.OK + pedido.toString();
-
-       }catch (Exception e){
-           return HttpStatus.BAD_REQUEST.toString();
-       }
-    }
+//    public String post(Pedido pedido){
+//       try{
+//           if(pedido.getEmail() == null){
+//               return HttpStatus.BAD_REQUEST.toString();
+//           }
+//           shoppingCartRepository.save(pedido);
+//           return HttpStatus.OK + pedido.toString();
+//
+//       }catch (Exception e){
+//           return HttpStatus.BAD_REQUEST.toString();
+//       }
+//    }
 
     public String delete(Long id){
         try{
@@ -46,7 +50,7 @@ public class ShoppingCartService {
         }
     }
 
-    public String put(ShoppingCart pedido){
+    public String put(Pedido pedido){
         try {
             shoppingCartRepository.save(pedido);
             return HttpStatus.OK.toString();
