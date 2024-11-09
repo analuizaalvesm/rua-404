6a49c38f80fc3d5820768260c6e632143dffed1e
analuizaalvesm <analuizaalvesm@gmail.com>
2024-11-09 19:18:15 -0300

    dashboard test

--- Codigo/rua-404-web/src/pages/Admin/Dashboard/Dashboard.tsx
+++ Codigo/rua-404-web/src/pages/Admin/Dashboard/Dashboard.tsx
@@ -1,5 +1,147 @@
-const Dashboard = () => {
-  return <div></div>;
+import { Calendar, Loader, Settings } from "lucide-react";
+import { ReactNode, useEffect, useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import DefaultLayout from "./DefaultLayout";
+import PageTitle from "@/components/admin/components/PageTitle";
+import ECommerce from "../Dashboard copy/ECommerce";
+import Profile from "../Profile";
+import FormElements from "../Form/FormElements";
+import FormLayout from "../Form/FormLayout";
+import Tables from "../Tables";
+import Chart from "../Chart";
+import Alerts from "../UiElements/Alerts";
+import Buttons from "../UiElements/Buttons";
+import SignIn from "../Authentication/SignIn";
+import SignUp from "../Authentication/SignUp";
+
+const AdminDashboard = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <DefaultLayout>
+      <Routes>
+        <Route
+          index
+          element={
+            <>
+              <PageTitle title="eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <ECommerce />
+            </>
+          }
+        />
+        <Route
+          path="/calendar"
+          element={
+            <>
+              <PageTitle title="Calendar | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Calendar />
+            </>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <>
+              <PageTitle title="Profile | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Profile />
+            </>
+          }
+        />
+        <Route
+          path="/forms/form-elements"
+          element={
+            <>
+              <PageTitle title="Form Elements | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <FormElements />
+            </>
+          }
+        />
+        <Route
+          path="/forms/form-layout"
+          element={
+            <>
+              <PageTitle title="Form Layout | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <FormLayout />
+            </>
+          }
+        />
+        <Route
+          path="/tables"
+          element={
+            <>
+              <PageTitle title="Tables | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Tables />
+            </>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <>
+              <PageTitle title="Settings | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Settings />
+            </>
+          }
+        />
+        <Route
+          path="/chart"
+          element={
+            <>
+              <PageTitle title="Basic Chart | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Chart />
+            </>
+          }
+        />
+        <Route
+          path="/ui/alerts"
+          element={
+            <>
+              <PageTitle title="Alerts | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Alerts />
+            </>
+          }
+        />
+        <Route
+          path="/ui/buttons"
+          element={
+            <>
+              <PageTitle title="Buttons | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Buttons />
+            </>
+          }
+        />
+        <Route
+          path="/auth/signin"
+          element={
+            <>
+              <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <SignIn />
+            </>
+          }
+        />
+        <Route
+          path="/auth/signup"
+          element={
+            <>
+              <PageTitle title="Signup | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <SignUp />
+            </>
+          }
+        />
+      </Routes>
+    </DefaultLayout>
+  );
 };
 
-export default Dashboard;
+export default AdminDashboard;
