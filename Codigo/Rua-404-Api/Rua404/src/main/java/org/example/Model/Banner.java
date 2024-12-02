c2673526dd544c6fdd489ad4396a1ee507dd8ec8
Matheus Porto <matheus5299porto@hotmail.com>
2024-12-02 09:02:06 -0300

    Adicionando rotas do banner

---
+++ Codigo/Rua-404-Api/Rua404/src/main/java/org/example/Model/Banner.java
@@ -0,0 +1,31 @@
+package org.example.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "home")
+public class Banner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_banner")
+    private Long id;
+
+    @Column(name = "texto")
+    private String texto;
+
+    @Column(name = "descricao")
+    private String descricao;
+}
---
+++ Codigo/Rua-404-Api/Rua404/src/main/java/org/example/Repositories/BannerRepository.java
@@ -0,0 +1,10 @@
+package org.example.Repositories;
+
+import org.example.Model.Banner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BannerRepository extends JpaRepository<Banner,Long> {
+
+}
